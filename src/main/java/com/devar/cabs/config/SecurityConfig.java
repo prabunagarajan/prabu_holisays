1ff3ac86247f957881d70e043f8ec4f7c6076130
Prabu <117503524+prabunagarajan@users.noreply.github.com>
2024-08-30 17:35:41 +0530

    Update SecurityConfig.java

--- src/main/java/com/devar/cabs/config/SecurityConfig.java
+++ src/main/java/com/devar/cabs/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 	    }
 
+	    
 	    @Override
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http.cors().and().csrf().disable()
@@ -50,10 +51,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	            .authorizeRequests()
 	            .antMatchers(HttpMethod.POST, "/api/users/login").permitAll()
 	            .antMatchers(HttpMethod.POST, "/api/users/signup").permitAll()
-	            .antMatchers("/swagger-ui.html", "/v2/api-docs", "/webjars/**", "/swagger-resources/**").permitAll()
-	            .anyRequest().authenticated().and()
-	            .logout().invalidateHttpSession(true).clearAuthentication(true)
-	            .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
+	            .antMatchers("/authentication/**", "/actuator/info/**", "/actuator/**", "/v2/api-docs",
+						"/swagger-resources", "/swagger-resources/**", "/validatorUrl", "/swagger-ui.html",
+						"/webjars/**", "/hystrix/**", "/hystrix", "*.stream", "/hystrix.stream", "/proxy.stream",
+						"/autuator/refresh", "/refresh", "/refresh/**")
+	            .permitAll().anyRequest().authenticated().and().logout().invalidateHttpSession(true)
+				.clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll();
 
 	        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	    }
