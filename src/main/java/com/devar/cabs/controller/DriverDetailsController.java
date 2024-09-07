e90b0580859964d50e582661c209b7aa87ac5505
Prabu <prabunagarajan30@gmail.com>
2024-09-07 13:04:39 +0530

    --crossorigin

--- src/main/java/com/devar/cabs/controller/DriverDetailsController.java
+++ src/main/java/com/devar/cabs/controller/DriverDetailsController.java
@@ -10,14 +10,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.devar.cabs.requestDTO.DriverDetailsRequestDTO;
@@ -40,6 +32,7 @@ import io.swagger.annotations.ApiResponses;
 		@ApiResponse(code = 409, message = "Conflict occurred") })
 
 @RequestMapping("/DriverDetails/")
+@CrossOrigin("*")
 public class DriverDetailsController {
 
 	@Autowired
--- src/main/java/com/devar/cabs/controller/VehicleDetailsController.java
+++ src/main/java/com/devar/cabs/controller/VehicleDetailsController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import io.swagger.annotations.ApiResponses;
 		@ApiResponse(code = 409, message = "Conflict occurred") })
 
 @RequestMapping("/vehicleDetails/")
+@CrossOrigin("*")
 public class VehicleDetailsController {
 
 	@Autowired
