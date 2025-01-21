124b3700cb325ff8aa1f9c31a867aec518232af0
Prabu <prabunagarajan30@gmail.com>
2025-01-21 10:27:20 +0530

    vehicleD add validation changed

--- src/main/java/com/devar/cabs/service/impl/VehicleDetailsServiceImpl.java
+++ src/main/java/com/devar/cabs/service/impl/VehicleDetailsServiceImpl.java
@@ -52,8 +52,10 @@ public class VehicleDetailsServiceImpl implements VehicleDetailsService {
 		Optional<VehicleDetailsEntity> vehiclenum = vehicleDetailsRepository
 				.findByVehicleNumber(vehicleDetailsRequestDTO.getVehicleNumber());
 		if (vehiclenum.isPresent()) {
-			return Library.getFailResponseCode(ErrorCode.INVALID_DATA.getErrorCode(),
-					ResponseMessageConstant.ALREADY_EXISTS.getMessage(new Object[] { "Vehicle Number" }));
+//			return Library.getFailResponseCode(ErrorCode.INVALID_DATA.getErrorCode(),
+//					ResponseMessageConstant.ALREADY_EXISTS.getMessage("Vehicle Number"));
+			return Library.getFailResponseCode(ErrorCode.FAILURE_RESPONSE.getErrorCode(),
+					"Record already exists for vehicle no: " + vehicleDetailsRequestDTO.getVehicleNumber());
 		} else {
 			VehicleDetailsEntity vehicleDetailsEntity = new VehicleDetailsEntity();
 			vehicleDetailsEntity.setVehicleNumber(vehicleDetailsRequestDTO.getVehicleNumber());
